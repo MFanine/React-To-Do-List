3b7e0fd49eb274cd892eb43834e71707c4a9857c
mouad fanine <mouadfanine@gmail.com>
2024-01-22 02:00:18 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import {useRef, useState} from "react";
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const inputRef = useRef()
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value;
+    const newItemv = {comleted: false , text}
+    setTodos([...todos, newItemv])
+    inputRef.current.value = "";
+  }
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].comleted= !newTodos[index].comleted
+    setTodos(newTodos);
+  }
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1)
+    setTodos(newTodos)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2>
+      <div className="to-do-container">
+      <ul>
+        {todos.map(({comleted ,text} , index) => {
+          return (
+          <div className="item">
+            <li className={comleted ? "done": ""} key={index}  ><input type="checkbox" className="cbox"  onClick={() => handleItemDone(index)}/> {text}</li>
+            <span onClick={() => handleDeleteItem(index)} className="trash">
+            ❌
+            </span>
+            </div>
+          );
+        })}
+      </ul>
+      <input ref={inputRef} placeholder="Enter item ..." className="input-items"/>
+      <button onClick={handleAddTodo} className="button-add">Add</button>
+      </div>
     </div>
   );
 }
